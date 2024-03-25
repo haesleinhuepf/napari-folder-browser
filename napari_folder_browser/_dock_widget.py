9b472fe40d64870dc638542257e8c0121fe2c297
Maximilian Schulz <maximilian.schulz@roche.com>
2024-03-25 10:17:31 +0100

    Fix file name filtering

--- napari_folder_browser/_dock_widget.py
+++ napari_folder_browser/_dock_widget.py
@@ -53,7 +53,6 @@ class FolderBrowser(QWidget):
     The napari viewer is passed in as an argument to the constructor
     """
     viewer: Viewer
-    current_directory: Path
     folder_chooser: FileEdit
     file_system_model: QFileSystemModel
     proxy_model: DirectoryFriendlyFilterProxyModel
@@ -68,17 +67,21 @@ class FolderBrowser(QWidget):
 
         # --------------------------------------------
         # Directory selection
-        self.current_directory: Path = Path(QFileDialog.getExistingDirectory(self, "Select Directory"))
+        current_directory: Path = Path(QFileDialog.getExistingDirectory(self, "Select Directory"))
         self.layout().addWidget(QLabel("Directory"))
         self.folder_chooser = FileEdit(
             mode=FileDialogMode.EXISTING_DIRECTORY,
-            value=self.current_directory,
+            value=current_directory,
         )
         self.layout().addWidget(self.folder_chooser.native)
 
         def directory_changed(*_) -> None:
-            self.current_directory = Path(self.folder_chooser.value)
-            self.tree_view.setRootIndex(self.file_system_model.index(self.current_directory.as_posix()))
+            current_directory = Path(self.folder_chooser.value)
+            self.tree_view.setRootIndex(
+                self.proxy_model.mapFromSource(
+                    self.file_system_model.index(current_directory.as_posix())
+                )
+            )
 
         self.folder_chooser.line_edit.changed.connect(directory_changed)
 
@@ -91,11 +94,11 @@ class FolderBrowser(QWidget):
 
         # Create search box and connect to proxy model
         self.layout().addWidget(QLabel("File filter"))
-        self.search_field = QLineEdit("*")
+        self.search_field = QLineEdit()
         # Note: We should agree on the best regex interaction to provide here
-        def update_filder(self, text: str) -> None:
+        def update_filter(text: str) -> None:
             self.proxy_model.setFilterRegExp(QRegExp(text, Qt.CaseInsensitive))
-        self.search_field.textChanged.connect(update_filder)
+        self.search_field.textChanged.connect(update_filter)
         search_widget = QWidget()
         search_widget.setLayout(QHBoxLayout())
         search_widget.layout().addWidget(QLabel("Search:"))
@@ -105,17 +108,22 @@ class FolderBrowser(QWidget):
         # Tree view and image selection
         self.tree_view = QTreeView()
         self.tree_view.setModel(self.proxy_model)
-        self.tree_view.setRootIndex(self.file_system_model.index(self.current_directory.as_posix()))
-        
+        # self.tree_view.setRootIndex(self.file_system_model.index(self.current_directory.as_posix()))
+        self.tree_view.setRootIndex(
+            self.proxy_model.mapFromSource(
+                self.file_system_model.index(current_directory.as_posix())
+            )
+        )
+    
         # Enable selecting multiple files for stack viewing (with shift/ctrl)
         self.tree_view.setSelectionMode(QAbstractItemView.ExtendedSelection)
 
         # Connect the double click signal to a slot that opens the file
-        self.tree_view.doubleClicked.connect(self.tree_double_click)
+        self.tree_view.doubleClicked.connect(self.__tree_double_click)
 
         # Enable context menu for multi selection
         self.tree_view.setContextMenuPolicy(Qt.CustomContextMenu)
-        self.tree_view.customContextMenuRequested.connect(self.show_context_menu)
+        self.tree_view.customContextMenuRequested.connect(self.__show_context_menu)
 
         # Hide the columns for size, type, and last modified
         self.tree_view.setHeaderHidden(True)
@@ -125,34 +133,39 @@ class FolderBrowser(QWidget):
         
         self.layout().addWidget(self.tree_view)
 
-    def tree_double_click(self, index: QModelIndex) -> None:
+    def __tree_double_click(self, index: QModelIndex) -> None:
         """Action on double click in the tree model
         
         Opens the selected file or in the folder
+        
+        Args:
+            index: Index of the selected item in the tree view
         """
-        file_path: str = self.file_system_model.filePath(index)
-        if self.file_system_model.isDir(index):
+        # Note: Need to remap indices from proxy to source for file system operations
+        source_index: QModelIndex = self.proxy_model.mapToSource(index)
+        file_path: str = self.file_system_model.filePath(source_index)
+        if self.file_system_model.isDir(source_index):
             self.tree_view.setRootIndex(index)
             self.folder_chooser.value = file_path
         else:
             print(f"Opening file: {file_path}")
             self.viewer.open(file_path)
 
-    def show_context_menu(self, position: QPoint) -> None:
+    def __show_context_menu(self, position: QPoint) -> None:
         """Show a context menu when right-clicking in the tree view"""
         menu = QMenu()
         open_multiple_action: QAction = menu.addAction("Open multiple files")
         open_multiple_action.triggered.connect(
-            lambda: self.open_multi_selection(is_stack=False)
+            lambda: self.__open_multi_selection(is_stack=False)
         )
         open_as_stack_action: QAction = menu.addAction("Open as stack")
         open_as_stack_action.triggered.connect(
-            lambda: self.open_multi_selection(is_stack=True)
+            lambda: self.__open_multi_selection(is_stack=True)
         )
         # Show the menu at the cursor position
         menu.exec_(self.tree_view.viewport().mapToGlobal(position))
 
-    def open_multi_selection(self, is_stack: bool) -> None:
+    def __open_multi_selection(self, is_stack: bool) -> None:
         """Open multiple files in the viewer
         
         The files are selected in the tree view
